48db2e71bdbcc9a6d8f6b1c0b2ce46deec5eb5d2
Simone Sabbatini <sabbatini.simone@gmail.com>
2017-08-06 12:55:36 +0100

    observable click handler

--- src/app/ui-dialer/ui-dialer.component.ts
+++ src/app/ui-dialer/ui-dialer.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import * as _ from 'lodash';
-import { Observable } from "rxjs/Observable";
+import { Observable } from "rxjs/Rx";
 
 @Component({
   selector: 'ui-dialer',
@@ -8,6 +8,9 @@ import { Observable } from "rxjs/Observable";
   styleUrls: ['./ui-dialer.component.scss']
 })
 export class UiDialerComponent implements OnInit {
+
+  @ViewChild('dial_button') dial_button$;
+  @ViewChild('hangup_button') hangup_button$;
   
   numbers: Array<string> = ['1','2','3','4','5','6','7','8','9','*','0','#'];
   number_to_dial: string = "";
@@ -16,18 +19,25 @@ export class UiDialerComponent implements OnInit {
 	
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() {
+
+    Observable.fromEvent(this.dial_button$.nativeElement, 'click')
+      .subscribe(()=>this.startCall());
+
+    Observable.fromEvent(this.hangup_button$.nativeElement, 'click')
+      .subscribe(()=>this.stopCall());
+  }
 
   onClickNumber(number: string) {
     this.number_to_dial += number;
   }
 
-  onDialClick() {
+  startCall() {
     this.is_on_call = true;
     this.timer = "00.01";
   }
 
-  onHangUpClick() {
+  stopCall() {
     this.is_on_call = false;
     this.timer = "";
   }
